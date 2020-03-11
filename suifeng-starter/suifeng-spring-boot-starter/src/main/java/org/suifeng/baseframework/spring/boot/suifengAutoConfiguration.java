124a861746e63c6e6dd1b8a12a77cb2ecee20a25
luoxc <lxc3219@163.com>
2020-03-11 15:07:56 +0800

    编写自动配置器

---
+++ suifeng-starter/suifeng-spring-boot-starter/src/main/java/org/suifeng/baseframework/spring/boot/suifengAutoConfiguration.java
@@ -0,0 +1,12 @@
+package org.suifeng.baseframework.spring.boot;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 业务基础框架自动配置器
+ * @author luoxc
+ */
+@Configuration
+public class suifengAutoConfiguration {
+
+}
