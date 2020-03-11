f963c45883aeaf20c7f8541f8895da352fd494a6
luoxc <lxc3219@163.com>
2020-03-11 11:38:34 +0800

    新增搜索、自动配置器模块

---
+++ suifeng-starter/suifeng-spring-boot-starter/src/test/java/org/suifeng/baseframework/starter/SuifengSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.suifeng.baseframework.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuifengSpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
